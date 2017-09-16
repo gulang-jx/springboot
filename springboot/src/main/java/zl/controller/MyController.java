72328992fa69521e7b8026d1cd8c4288a61c9aa4
LAPTOP-8E36O67B\zhouliang <1719939724@qq.com>
2017-09-16 22:16:47 +0800

    springboot

---
+++ springboot/src/main/java/zl/controller/MyController.java
@@ -0,0 +1,15 @@
+package zl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping
+	@ResponseBody
+	public String helloWorld(){
+		return "Hello world";
+	}
+}
